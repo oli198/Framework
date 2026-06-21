1575aef5ea9798646b0978542012b1fba57030c9
oli198 <kalapoly44@gmail.com>
2026-06-21 22:34:44 +0300

    sprint1 ity fa lasa tao amn sprint0

---
+++ src/main/java/framework/util/ClassScanner.java
@@ -0,0 +1,59 @@
+package framework.util;
+
+import framework.annotation.Controller;
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
+public class ClassScanner {
+
+    /**
+     * Liste toutes les classes dans un package donné
+     */
+    public static List<Class<?>> getClassesInPackage(String packageName) throws Exception {
+        List<Class<?>> classes = new ArrayList<>();
+
+        String path = packageName.replace('.', '/');
+        URL resource = Thread.currentThread()
+                             .getContextClassLoader()
+                             .getResource(path);
+
+        if (resource == null) {
+            throw new Exception("Package introuvable : " + packageName);
+        }
+
+        File directory = new File(resource.toURI());
+
+        for (File file : directory.listFiles()) {
+            if (file.getName().endsWith(".class")) {
+                String className = packageName + "." 
+                                 + file.getName().replace(".class", "");
+                classes.add(Class.forName(className));
+            }
+        }
+
+        return classes;
+    }
+
+    /**
+     * Vérifie si une classe possède l'annotation @Controller
+     */
+    public static boolean hasControllerAnnotation(Class<?> clazz) {
+        return clazz.isAnnotationPresent(Controller.class);
+    }
+
+    /**
+     * Retourne uniquement les classes annotées @Controller dans un package
+     */
+    public static List<Class<?>> getControllers(String packageName) throws Exception {
+        List<Class<?>> controllers = new ArrayList<>();
+
+        for (Class<?> clazz : getClassesInPackage(packageName)) {
+            if (hasControllerAnnotation(clazz)) {
+                controllers.add(clazz);
+            }
+        }
+
+        return controllers;
+    }
+}
